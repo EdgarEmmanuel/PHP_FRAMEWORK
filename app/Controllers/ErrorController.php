07f7655b367b8989e99d0e63b163f660df6210ca
Edgar <mmnledgar@gmail.com>
2021-08-14 15:22:40 +0000

    update the ErrorController

--- app/Controllers/ErrorController.php
+++ app/Controllers/ErrorController.php
@@ -6,14 +6,14 @@ namespace App\Controllers;
 class ErrorController extends BaseController {
 
     public function PageNotFound($notFoundUrl){
-        $text = "URL <b> <?=$this->e($notFoundUrl)?> </b> Not Found.<br/>
+        $text = "URL <b> {$notFoundUrl} </b> Not Found.<br/>
         See the Route Declaration file ";
         $this->renderTheTemplate($text);
     }
 
     public function controllerOrMethodNotFound($controllerNotFounded, $isTheMethod = false ){
         $text = $isTheMethod ? "Function" : "Controller ";
-        $message = "The {$text} <b> $controllerNotFounded </b> Not Found";
+        $message = "The {$text} <b> {$controllerNotFounded} </b> Not Found";
         $this->renderTheTemplate($message);
     }
 
